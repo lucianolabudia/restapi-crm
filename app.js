947b68719df10dfbef1387143ba346f8a101ec75
Luciano Labudia <labudia.luciano@gmail.com>
2023-02-19 11:41:18 -0300

    Creating project and connecting to MongoDB

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+
+// conectar mongo
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/restapis', {
+    useNewUrlParser: true
+});
+
+// crear el servidor
+const app = express();
+
+// Rutas de la app
+app.use('/', routes());
+
+// puerto
+app.listen(5000);0
